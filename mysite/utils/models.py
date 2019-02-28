90c63fc8df315ccc7a474d6f4b7d8e008281d4ae
Frances Botsford <frances@switchingprotocols.com>
2019-02-28 17:04:05 -0500

    add themeable social links and css

--- mysite/utils/models.py
+++ mysite/utils/models.py
@@ -8,10 +8,30 @@ class SocialMediaSettings(BaseSetting):
     class Meta:
         verbose_name = 'social media accounts'
     facebook = models.URLField(
-        help_text='Facebook page URL')
+        help_text='Facebook page URL',
+        blank=True
+    )
     instagram = models.CharField(
-        max_length=255, help_text='Instagram username, without the @')
+        max_length=255,
+        help_text='Instagram username, without the @',
+        blank=True
+    )
     twitter = models.CharField(
-        max_length=255, help_text='Your twitter handle without the @')
+        max_length=255,
+        help_text='Your twitter handle without the @',
+        blank=True
+    )
     youtube = models.URLField(
-        help_text='Your YouTube channel or user account URL')
+        help_text='Your YouTube channel or user account URL',
+        blank=True
+    )
+
+@register_setting
+class theme(BaseSetting):
+    class Meta:
+        verbose_name = 'theme'
+    css = models.CharField(
+        help_text='theme css',
+        max_length=255,
+        blank=True
+    )
